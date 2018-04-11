9530e9679afb28d228533e4a6602d0c2dfb886ac
Claudio Scordino <claudio@evidence.eu.com>
2018-04-11 13:59:18 +0200

    aarch64: add support for Nvidia X2

---
+++ pkg/arch/aarch64/soc/nvidia_tegra_x2/ee_aarch64_gic.h
@@ -0,0 +1,61 @@
+/* ###*B*###
+ * Erika Enterprise, version 3
+ * 
+ * Copyright (C) 2017 Evidence s.r.l.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License, version 2, for more details.
+ * 
+ * You should have received a copy of the GNU General Public License,
+ * version 2, along with this program; if not, see
+ * <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html >.
+ * 
+ * This program is distributed to you subject to the following
+ * clarifications and special exceptions to the GNU General Public
+ * License, version 2.
+ * 
+ * THIRD PARTIES' MATERIALS
+ * 
+ * Certain materials included in this library are provided by third
+ * parties under licenses other than the GNU General Public License. You
+ * may only use, copy, link to, modify and redistribute this library
+ * following the terms of license indicated below for third parties'
+ * materials.
+ * 
+ * In case you make modified versions of this library which still include
+ * said third parties' materials, you are obligated to grant this special
+ * exception.
+ * 
+ * The complete list of Third party materials allowed with ERIKA
+ * Enterprise version 3, together with the terms and conditions of each
+ * license, is present in the file THIRDPARTY.TXT in the root of the
+ * project.
+ * ###*E*### */
+
+/** \file   ee_aarch64_gic.h
+ *  \brief  GIC addresses for NVIDIA Tegra X2
+ *
+ *  \note TO BE DOCUMENTED!!!
+ *
+ *  \author Claudio Scordino
+ *  \date   2018
+ */
+
+#if (!defined(OSEE_AARCH64_GIC_H))
+#define OSEE_AARCH64_GIC_H
+
+/* GIC base address for NVIDIA TEGRA X2 */
+#define OSEE_GIC_BASE     (0x3880000U)
+#define OSEE_GICD_OFFSET  (0x1000U)
+#define OSEE_GICC_OFFSET  (0x2000U)
+#define OSEE_GICH_OFFSET  (0x4000U)
+#define OSEE_GICV_OFFSET  (0x6000U)
+
+#endif /* OSEE_AARCH64_GIC_H */
