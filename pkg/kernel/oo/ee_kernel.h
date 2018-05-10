cbc165a9d5b2ff8f49285f780e405a3ffc44611e
Errico Guidieri <e.guidieri@evidence.eu.com>
2018-05-10 18:22:56 +0200

    Fix warning when PreTaskHook and PostTaskHook are active without ErrorHook

--- pkg/kernel/oo/ee_kernel.h
+++ pkg/kernel/oo/ee_kernel.h
@@ -264,6 +264,8 @@ LOCAL_INLINE FUNC(void, OS_CODE)
 #if (defined(OSEE_HAS_CONTEXT))
   VAR(OsEE_os_context, AUTOMATIC) prev_os_ctx = p_ccb->os_context;
   p_ccb->os_context = OSEE_PRETASKHOOK_CTX;
+#else
+  ((void)p_ccb);
 #endif /* OSEE_HAS_CONTEXT */
   PreTaskHook();
 #if (defined(OSEE_HAS_CONTEXT))
@@ -284,6 +286,8 @@ LOCAL_INLINE FUNC(void, OS_CODE)
 #if (defined(OSEE_HAS_CONTEXT))
   VAR(OsEE_os_context, AUTOMATIC) prev_os_ctx = p_ccb->os_context;
   p_ccb->os_context = OSEE_POSTTASKHOOK_CTX;
+#else
+  ((void)p_ccb);
 #endif /* OSEE_HAS_CONTEXT */
   PostTaskHook();
 #if (defined(OSEE_HAS_CONTEXT))
