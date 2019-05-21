002aebc090e525837a70ce9e3e80c4f14276a3b6
Errico Guidieri <e.guidieri@evidence.eu.com>
2019-05-21 16:08:49 +0200

    [x86-64] Update EtherCat template, to include local non-standar SOEM functions

--- pkg/arch/x86-64/examples/x86_64_xen_ethercat/main.c
+++ pkg/arch/x86-64/examples/x86_64_xen_ethercat/main.c
@@ -42,6 +42,9 @@
 #include "ee.h"
 #include "intel_i210.h"
 
+/* SOEM include */
+#include "oshw.h"
+
 /** timeout value in us for tx frame to return to rx */
 #define EC_TIMEOUTRET      2000
 
@@ -52,16 +55,21 @@
 // References:
 //	https://openethercatsociety.github.io/doc/soem/tutorial_8txt.html
 
+/* SOEM functions */
 int ec_init(const char *ifname);
 int ec_config_init(uint8_t usetable);
-extern int ec_slavecount;
 int ec_config_map(void *pIOmap);
-void print_slave_info (void);
-void set_operational (void);
 int ec_send_processdata(void);
 int ec_receive_processdata(int timeout);
-void set_output_int16 (uint16_t slave_nb, uint8_t module_index, int16_t value);
 
+/* SOEM var */
+extern int ec_slavecount;
+
+/* Local functions */
+static void print_slave_info (void);
+static void set_operational (void);
+static void set_output_int16 (uint16_t slave_nb, uint8_t module_index, 
+				int16_t value);
 
 void ee_usleep (uint32_t usec);
 void ee_usleep (uint32_t usec)
@@ -164,5 +172,78 @@ int main(void)
         return 0;
 }
 
+static void print_slave_info (void)
+{
+        OSEE_PRINT("Printing slave info for %d slaves...\n", ec_slavecount);
+        for (int i = 1; i < ec_slavecount + 1; ++i) {
+                OSEE_PRINT("Name: %s\n", ec_slave[i].name);
+                OSEE_PRINT("\tSlave nb: %d\n", i);
+                if ((ec_slave[i].state & 0x0f) == EC_STATE_NONE)
+                        OSEE_PRINT("\tState: NONE\n");
+                else if ((ec_slave[i].state & 0x0f) == EC_STATE_INIT)
+                        OSEE_PRINT("\tState: INIT\n");
+                else if ((ec_slave[i].state & 0x0f) == EC_STATE_PRE_OP)
+                        OSEE_PRINT("\tState: PRE_OP\n");
+                else if ((ec_slave[i].state & 0x0f) == EC_STATE_BOOT)
+                        OSEE_PRINT("\tState: BOOT\n");
+                else if ((ec_slave[i].state & 0x0f) == EC_STATE_SAFE_OP)
+                        OSEE_PRINT("\tState: SAFE_OP\n");
+                else if ((ec_slave[i].state & 0x0f) == EC_STATE_OPERATIONAL)
+                        OSEE_PRINT("\tState: OPERATIONAL\n");
+                if (ec_slave[i].state & 0x10)
+                        OSEE_PRINT("\tState: ACK or ERROR\n");
+                OSEE_PRINT("\tOutput bytes: %u\n", ec_slave[i].Obytes);
+                OSEE_PRINT("\tOutput bits: %u\n", ec_slave[i].Obits);
+                OSEE_PRINT("\tInput bytes: %u\n", ec_slave[i].Ibytes);
+                OSEE_PRINT("\tInput bits: %u\n", ec_slave[i].Ibits);
+                OSEE_PRINT("\tConfigured address: %u\n", ec_slave[i].configadr);
+                OSEE_PRINT("\tOutput address: %p\n", ec_slave[i].outputs);
+                OSEE_PRINT("\tOstartbit: %x\n", ec_slave[i].Ostartbit);
+                OSEE_PRINT("\tCoE details: %x\n", ec_slave[i].CoEdetails); // See ECT_COEDET_*
+                OSEE_PRINT("\tHas DC capability: %d\n\n", ec_slave[i].hasdc);
+        }
+}
 
+static void set_operational (void)
+{
+        int chk;
+        /* wait for all slaves to reach SAFE_OP state */
+        ec_statecheck(0, EC_STATE_SAFE_OP,  EC_TIMEOUTSTATE * 4);
 
+        ec_slave[0].state = EC_STATE_OPERATIONAL;
+        /* send one valid process data to make outputs in slaves happy*/
+        ec_send_processdata();
+        ec_receive_processdata(EC_TIMEOUTRET);
+        /* request OP state for all slaves */
+        ec_writestate(0);
+        chk = 40;
+        do {
+                ec_send_processdata();
+                ec_receive_processdata(EC_TIMEOUTRET);
+                ec_statecheck(0, EC_STATE_OPERATIONAL, 50000);
+        } while (chk-- && (ec_slave[0].state != EC_STATE_OPERATIONAL));
+
+        if (ec_slave[0].state == EC_STATE_OPERATIONAL ) {
+                for (int i = 1; i < ec_slavecount +1; ++i)
+                        ec_slave[i].state = EC_STATE_OPERATIONAL;
+                for (int i = 1; i < ec_slavecount +1; ++i)
+                        ec_writestate(i);
+                ec_send_processdata();
+                ec_receive_processdata(EC_TIMEOUTRET);
+        } else {
+                OSEE_PRINT("WARNING: cannot reach OPERATIONAL state\n");
+        }
+}
+
+static void set_output_int16 (uint16_t slave_nb, uint8_t module_index, int16_t value)
+{
+        uint8_t *data_ptr;
+
+        data_ptr = ec_slave[slave_nb].outputs;
+        /* Move pointer to correct module index*/
+        data_ptr += module_index * 2;
+        /* Read value byte by byte since all targets can't handle misaligned
+           addresses */
+        *data_ptr++ = (value >> 0) & 0xFF;
+        *data_ptr++ = (value >> 8) & 0xFF;
+}
